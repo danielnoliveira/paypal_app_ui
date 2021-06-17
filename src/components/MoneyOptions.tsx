cd68b159a45da8f9cc6b525c9e9b399cb2b8be95
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-06-17 15:09:15 -0300

    Componente de botão para o segundo container da tela HomeScreen

---
+++ src/components/MoneyOptions.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import MoneyButton from './MoneyButton';
+
+export default function MoneyOptions() {
+  return (
+    <View style={styles.container}>
+      <MoneyButton colored img={require('./../../assets/upload-alt1.png')} />
+      <MoneyButton img={require('./../../assets/upload-alt3.png')} />
+      <TouchableOpacity style={styles.optionsButton}>
+        <Image
+          style={{width: 32, height: 32}}
+          source={require('./../../assets/ellipsis-h1.png')}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  optionsButton: {
+    width: 66,
+    height: 120,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
