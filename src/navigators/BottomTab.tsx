b879afe3d98a2c4879acd9c4d46a9fcbf12b628d
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-06-17 15:05:57 -0300

    Criação da pasta navigators e do componente BottomTab

---
+++ src/navigators/BottomTab.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import {Image, Text, StyleSheet} from 'react-native';
+import ContactsScreen from '../screens/ContactsScreen';
+import WalletScreen from '../screens/WalletScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import IconBar from '../components/IconBar';
+
+const BottomTabs = createBottomTabNavigator();
+
+const Main = () => {
+  return (
+    <BottomTabs.Navigator
+      initialRouteName="Home"
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused}) => {
+          var icon: JSX.Element = <></>;
+          var style = focused ? {tintColor: '#0070BA'} : {tintColor: 'black'};
+          if (route.name === 'Home') {
+            icon = (
+              <Image
+                key={1}
+                source={require('./../../assets/home-alt1.png')}
+                style={style}
+              />
+            );
+          } else if (route.name === 'Contacts') {
+            icon = (
+              <Image
+                key={2}
+                source={require('./../../assets/users-alt1.png')}
+                style={style}
+              />
+            );
+          } else if (route.name === 'Wallet') {
+            icon = (
+              <Image
+                key={3}
+                source={require('./../../assets/wallet1.png')}
+                style={style}
+              />
+            );
+          } else if (route.name === 'Settings') {
+            icon = (
+              <Image
+                key={4}
+                source={require('./../../assets/setting1.png')}
+                style={style}
+              />
+            );
+          }
+
+          // You can return any component that you like here!
+          return (
+            <IconBar focused={focused}>
+              {[
+                icon,
+                <Text key={6} style={styles.textIconBar}>
+                  {route.name}
+                </Text>,
+              ]}
+            </IconBar>
+          );
+        },
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          paddingHorizontal: 5,
+          height: 70,
+        },
+      }}>
+      <BottomTabs.Screen name="Home" component={HomeScreen} />
+      <BottomTabs.Screen name="Contacts" component={ContactsScreen} />
+      <BottomTabs.Screen name="Wallet" component={WalletScreen} />
+      <BottomTabs.Screen name="Settings" component={SettingsScreen} />
+    </BottomTabs.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  textIconBar: {
+    fontSize: 18,
+    lineHeight: 32,
+    fontWeight: '600',
+    fontFamily: 'Roboto',
+    color: '#0070BA',
+    marginLeft: 6,
+  },
+});
+
+export default Main;
