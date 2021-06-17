8088afcdd6cf0399a5793c9b3123db879fc5cdfa
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-06-17 15:05:23 -0300

    Separação do elemento bottomTabNavigator para a pasta src/navigators

--- App.tsx
+++ App.tsx
@@ -8,111 +8,23 @@
  * @format
  */
 
-import React, {useState} from 'react';
-import {
-  Image,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
-import LinkButton from './src/components/LinkButton';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen';
+import Main from './src/navigators/BottomTab';
 
-const {width, height} = Dimensions.get('window');
+const Stack = createStackNavigator();
 
 const App = () => {
-  const [user, setUser] = useState('');
-  const [password, setPassword] = useState('');
   return (
-    <View style={styles.container}>
-      <StatusBar backgroundColor="transparent" barStyle="dark-content" />
-      <View style={styles.LogoPayPal}>
-        <Image
-          source={require('./assets/logo-mini.png')}
-          style={{marginRight: 16.95}}
-        />
-        <Image source={require('./assets/logo-large.png')} />
-      </View>
-      <View>
-        <TextInput
-          style={styles.input}
-          value={user}
-          onChangeText={text => setUser(text)}
-          placeholder="Enter your name or email"
-        />
-        <TextInput
-          style={styles.input}
-          value={password}
-          onChangeText={text => setPassword(text)}
-          placeholder="Passowrd"
-        />
-        <TouchableOpacity
-          style={styles.loginButton}
-          activeOpacity={0.8}
-          onPress={() => console.log('Botão apertado')}>
-          <LinearGradient
-            style={styles.gradient}
-            start={{x: 0, y: 0}}
-            end={{x: 1, y: 0}}
-            angle={90}
-            angleCenter={{x: 0.5, y: 0.5}}
-            colors={['#0070BA', '#1546A0']}>
-            <Text style={styles.loginButtonText}>Log in</Text>
-          </LinearGradient>
-        </TouchableOpacity>
-      </View>
-      <LinkButton
-        content="Having trouble logging in?"
-        cb={() => console.log('Login problems')}
-      />
-      <LinkButton content="Sign up" cb={() => console.log('Sign up')} />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator headerMode="none" initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Main" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  LogoPayPal: {
-    flexDirection: 'row',
-    marginTop: height * 0.16133004926,
-    marginBottom: height * 0.12133004926,
-    marginHorizontal: width * 0,
-    alignItems: 'baseline',
-  },
-  input: {
-    width: width * 0.696,
-    height: 56,
-    textAlign: 'center',
-    fontSize: 18,
-    lineHeight: 22.93,
-    fontWeight: '600',
-    fontFamily: 'Roboto',
-    borderRadius: 20,
-    marginBottom: 16,
-  },
-  loginButton: {
-    width: width * 0.696,
-    height: 64,
-    marginTop: 37,
-  },
-  loginButtonText: {
-    fontSize: 20,
-    lineHeight: 64,
-    fontWeight: '700',
-    fontFamily: 'Roboto',
-    color: 'white',
-    textAlign: 'center',
-  },
-  gradient: {
-    flex: 1,
-  },
-});
 
 export default App;
