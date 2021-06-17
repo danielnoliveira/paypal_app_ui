b485566e8aff6c49a8d6fe2adab7711e40243d0c
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-06-17 16:03:58 -0300

    Nova Stack Screen adicionada: ActivityScreen

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from './src/screens/LoginScreen';
 import Main from './src/navigators/BottomTab';
+import ActivityScreen from './src/screens/ActivityScreen';
 
 const Stack = createStackNavigator();
 
@@ -22,6 +23,7 @@ const App = () => {
       <Stack.Navigator headerMode="none" initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="Activity" component={ActivityScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
