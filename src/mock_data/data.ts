e7c3e5788935c21e602cdf5a55a6e5ae183f8f95
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-06-23 14:31:49 -0300

    pasta de dados mock

---
+++ src/mock_data/data.ts
@@ -0,0 +1,40 @@
+const activitys = [
+  {
+    name: 'Mike Rine',
+    timeOperation: '21/06/2021',
+    value: 250,
+    type: 'debit',
+  },
+  {
+    name: 'Google Drive',
+    timeOperation: '19/06/2021',
+    value: 138.5,
+    type: 'credit',
+  },
+  {
+    name: 'Casey Smith',
+    timeOperation: '19/06/2021',
+    value: 531,
+    type: 'debit',
+  },
+  {
+    name: 'Apple Store',
+    timeOperation: '18/06/2021',
+    value: 250,
+    type: 'credit',
+  },
+  {
+    name: 'Pizza Delivery',
+    timeOperation: '18/06/2021',
+    value: 58.9,
+    type: 'credit',
+  },
+  {
+    name: 'Amazon.com',
+    timeOperation: '18/06/2021',
+    value: 300,
+    type: 'credit',
+  },
+];
+
+export default activitys;
